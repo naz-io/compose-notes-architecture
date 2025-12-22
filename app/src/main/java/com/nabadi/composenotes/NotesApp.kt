089b793c882aee27aa9d728a0e9ecc36cc878bb2
Naz K. Abadi <naz@Nazs-MacBook-Air.local>
2025-12-22 11:58:20 -0500

    Initial project setup

---
+++ app/src/main/java/com/nabadi/composenotes/NotesApp.kt
@@ -0,0 +1,9 @@
+package com.nabadi.composenotes
+
+import androidx.compose.runtime.Composable
+import com.nabadi.composenotes.navigation.NotesNavHost
+
+@Composable
+fun NotesApp() {
+    NotesNavHost()
+}
---
+++ app/src/main/java/com/nabadi/composenotes/navigation/NotesNavHost.kt
@@ -0,0 +1,8 @@
+package com.nabadi.composenotes.navigation
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun NotesNavHost() {
+    // NavHost will live here later
+}
