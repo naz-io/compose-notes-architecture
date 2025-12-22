244bb2fda29a492d4aa18dbe5bed507a83ed3649
naz-io <nazanin.k.abadi@gmail.com>
2025-12-22 16:26:28 -0500

    Add Compose Navigation

--- app/src/main/java/com/nabadi/composenotes/navigation/NotesNavGraph.kt
+++ app/src/main/java/com/nabadi/composenotes/navigation/NotesNavGraph.kt
@@ -2,10 +2,27 @@ package com.nabadi.composenotes.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.nabadi.composenotes.ui.list.NotesListRoute
+
+object Routes {
+    // Single destination only
+    const val NOTES_LIST = "notes_list"
+}
 
 @Composable
 fun NotesNavGraph(modifier: Modifier = Modifier) {
-    // Single destination only
-    NotesListRoute(modifier = modifier)
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = Routes.NOTES_LIST,
+        modifier = modifier
+    ) {
+        composable(Routes.NOTES_LIST) {
+            NotesListRoute()
+        }
+    }
 }
